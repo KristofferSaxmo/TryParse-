a3262c19d55834497e4dd4108d4818c27237cfc2
KristofferSaxmo <54435541+KristofferSaxmo@users.noreply.github.com>
2020-11-17 10:58:06 +0100

    Add files via upload

---
+++ TryParse()/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryParse__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int resultat;
+            if (!TryParse(Console.ReadLine(), out resultat))
+                Console.WriteLine("Skriv in en siffra");
+        }
+        public static bool TryParse(string s, out int resultat)
+        {
+            try
+            {
+                resultat = int.Parse(s);
+                return true;
+            }
+            catch
+            {
+                resultat = 0;
+                return false;
+            }
+        }
+    }
+}
